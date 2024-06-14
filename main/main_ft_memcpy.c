e907522e71a986d74cae2206a147858b58d4f89b
Javier Álvarez Villa <javieralvarezvilla@gmail.com>
2024-06-14 20:31:22 +0200

    3

--- main/main_ft_memcpy.c
+++ main/main_ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 12:04:02 by javialva          #+#    #+#             */
-/*   Updated: 2024/06/14 12:20:03 by javialva         ###   ########.fr       */
+/*   Updated: 2024/06/14 20:30:34 by javialva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -18,16 +18,17 @@ int	main(void)
 {
 	char	*src = "Hola a todos";
 	char	dest[20];
-	int	i = 0;
+	size_t	i = 0;
+	size_t	n = 4;
 	while (src[i] != '\0')
 	{
 		write(1, &src[i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
-	ft_memcpy(dest, src, 4);
+	ft_memcpy(dest, src, n);
 	i = 0;
-	while (dest[i] != '\0')
+	while (i < n)
 	{
 		write(1, &dest[i], 1);
 		i++;
